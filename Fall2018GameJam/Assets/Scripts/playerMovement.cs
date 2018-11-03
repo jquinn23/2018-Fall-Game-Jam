833bd617d0854d7d1810bfc578e37b2d5dcde4da
Mist/Jonathan <quinnjonathan402@gmail.com>
2018-11-02 21:01:35 -0400

    Finished player movement

---
+++ Fall2018GameJam/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+
+    Rigidbody2D playerRigidBody;
+    public float speed;
+    
+    void Start () {
+        playerRigidBody = this.GetComponent<Rigidbody2D>();
+	}
+
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+        transform.position = transform.position + movement * speed * Time.deltaTime;
+    }
+
+}
